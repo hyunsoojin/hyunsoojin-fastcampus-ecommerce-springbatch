d2e6f0ec90d0fb09b54a62b9d4101251000f90eb
Jinny <jhs7251@naver.com>
2025-04-14 21:54:41 +0900

    commit ecommerce-springbatch

--- ecommerce-batch/src/main/java/fastcampus/ecommerce/batch/BatchApplication.java
+++ ecommerce-batch/src/main/java/fastcampus/ecommerce/batch/BatchApplication.java
@@ -1,7 +1,12 @@
 package fastcampus.ecommerce.batch;
 
+import io.prometheus.client.exporter.PushGateway;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication
 public class BatchApplication {
@@ -9,4 +14,24 @@ public class BatchApplication {
   public static void main(String[] args) {
     SpringApplication.run(BatchApplication.class, args);
   }
+
+  @Bean
+  public PushGateway pushGateway(
+      @Value("${prometheus.pushgateway.url:localhost:9091}") String url
+  ) {
+    return new PushGateway(url);
+  }
+
+  @Bean
+  public TaskExecutor taskExecutor() {
+    ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+    executor.setCorePoolSize(128);
+    executor.setMaxPoolSize(128);
+    executor.setQueueCapacity(128);
+    executor.setAllowCoreThreadTimeOut(true);
+    executor.setWaitForTasksToCompleteOnShutdown(true);
+    executor.setAwaitTerminationSeconds(10);
+    return executor;
+  }
+
 }
